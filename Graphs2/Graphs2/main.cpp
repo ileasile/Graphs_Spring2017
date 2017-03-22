0c32796dcb91c1baa1172e1c8acfaa0106088c11
ileasile <leopardile@yandex.ru>
2017-03-22 15:28:02 +0300

    Привёл код к нормальному виду

--- Graphs2/Graphs2/main.cpp
+++ Graphs2/Graphs2/main.cpp
@@ -1,4 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define notset(p, i, j) (i < 0 || j < 0 || i >= N || j >= N || g[p][i][j])
+#define set_used(x0, y0, x1, y1) (used[x0][y0][x1][y1 / 8] |= mask[y1 % 8])
+#define not_used(x0, y0, x1, y1) (!(used[x0][y0][x1][y1 / 8] & mask[y1 % 8]))
 
 #include <iostream>
 #include <fstream>
@@ -10,14 +13,6 @@
 #include <cstdlib>
 
 using namespace std;
-
-int N;
-int x0i, x1i, y0i, y1i;
-char x0s, x1s, y0s, y1s;
-bool g[2][128][128];
-unsigned char used[128][128][128][13];
-
-
 struct vert {
 	char x0, y0, x1, y1;
 };
@@ -25,8 +20,20 @@ struct QueueElem {
 	vert v;
 	short d;
 };
-inline bool notset(char p, char i, char j) {
-	return i < 0 || j < 0 || i >= N || j >= N || g[p][i][j];
+
+int N;
+int x0i, x1i, y0i, y1i;
+char x0s, x1s, y0s, y1s;
+unsigned char used[128][128][128][13];
+bool g[2][128][128];
+unsigned char mask[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+queue<QueueElem> q;
+
+inline void set_if_not_used(char & x0, char & y0, char & x1, char & y1, short & d) {
+	if (not_used(x0, y0, x1, y1)) {
+		set_used(x0, y0, x1, y1);
+		q.push({ vert{ x0, y0, x1, y1 }, d });
+	}
 }
 
 int main() {
@@ -37,25 +44,16 @@ int main() {
 	ifstream in("input.txt");
 	in >> N >> x0i >> y0i >> x1i >> y1i;
 	x0s = x0i; x1s = x1i; y0s = y0i; y1s = y1i;
-	for (int p = 0; p < 2; ++p) {
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
+	for (int p = 0; p < 2; ++p)
+		for (int i = 0; i < N; ++i)
+			for (int j = 0; j < N; ++j)
 				in >> g[p][i][j];
-			}
-		}
-	}
 	in.close();
-
-	unsigned char mask[8];
-	mask[0] = 1;
-	for (int i = 1; i < 8; ++i) mask[i] = mask[i - 1] << 1;
-
-	queue<QueueElem> q;
 
 	auto vs = vert{ x0s, y0s, x1s, y1s };
 	q.push({ vs, 0 });
-	used[x0s][y0s][x1s][y1s / 8] |= mask[y1s % 8];
-
+	set_used(x0s, y0s, x1s, y1s);
+	
 	while (!q.empty()) {
 		QueueElem qe;
 		qe = q.front();
@@ -86,22 +84,10 @@ int main() {
 		if (notset(1, qe.v.x1, dy1)) dy1 = qe.v.y1;
 
 		++qe.d;
-		if (!(used[lx0][qe.v.y0][lx1][qe.v.y1 / 8] & mask[qe.v.y1 % 8])) {
-			used[lx0][qe.v.y0][lx1][qe.v.y1 / 8] |= mask[qe.v.y1 % 8];
-			q.push({ vert{ lx0, qe.v.y0, lx1, qe.v.y1 }, qe.d });
-		}
-		if (!(used[rx0][qe.v.y0][rx1][qe.v.y1 / 8] & mask[qe.v.y1 % 8])) {
-			used[rx0][qe.v.y0][rx1][qe.v.y1 / 8] |= mask[qe.v.y1 % 8];
-			q.push({ vert{ rx0, qe.v.y0, rx1, qe.v.y1 }, qe.d });
-		}
-		if (!(used[qe.v.x0][uy0][qe.v.x1][uy1 / 8] & mask[uy1 % 8])) {
-			used[qe.v.x0][uy0][qe.v.x1][uy1 / 8] |= mask[uy1 % 8];
-			q.push({ vert{ qe.v.x0, uy0, qe.v.x1, uy1 }, qe.d });
-		}
-		if (!(used[qe.v.x0][dy0][qe.v.x1][dy1 / 8] & mask[dy1 % 8])) {
-			used[qe.v.x0][dy0][qe.v.x1][dy1 / 8] |= mask[dy1 % 8];
-			q.push({ vert{ qe.v.x0, dy0, qe.v.x1, dy1 }, qe.d });
-		}
+		set_if_not_used(lx0, qe.v.y0, lx1, qe.v.y1, qe.d);
+		set_if_not_used(rx0, qe.v.y0, rx1, qe.v.y1, qe.d);
+		set_if_not_used(qe.v.x0, uy0, qe.v.x1, uy1, qe.d);
+		set_if_not_used(qe.v.x0, dy0, qe.v.x1, dy1, qe.d);
 	}
 
 	return 0;
