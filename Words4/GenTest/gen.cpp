ce29aaa68faa1f7479e7a30019115cd9e48ce203
ileasile <leopardile@yandex.ru>
2017-04-17 00:51:05 +0300

    Сгенерировал другой тест

--- Words4/GenTest/gen.cpp
+++ Words4/GenTest/gen.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <ctime>
@@ -9,19 +10,34 @@
 using namespace std;
 
 int main() {
-	ofstream f("input.txt");
+	ofstream f("../Graphs4/input.txt");
+	vector<string> v;
+
 	int V, H;
 
 	V = 1000;
 	H = 1000;
 
 	f << V << " " << H << " \n";
+
+	v.resize(V);
 	for (int i = 1; i <= V; ++i) {
-		f << i*i << " 0 999999 \n";
+		v[i - 1] = to_string(i*i) + " " + "0 999999" + "\n";
 	}
-	for (int i = 1; i <= H; ++i) {
-		f << "0 999999 " << i*i << " \n";
+	std::random_shuffle(v.begin(), v.end());
+	for (auto & s : v) {
+		f << s;
 	}
 
+
+	for (int i = 1; i <= H; ++i) {
+		v[i - 1] =  "0 999999 "  + to_string(i*i)  + "\n";
+	}
+	std::random_shuffle(v.begin(), v.end());
+	for (auto & s : v) {
+		f << s;
+	}
+
+	f.close();
 	return 0;
 }
